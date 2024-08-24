2285ecf4732336838fe17def8c6aee2396c0c22d
BrandonK7200 <brandonkillman@hotmail.com>
2024-08-23 21:03:10 -0300

    WIP Cliffside & Pebblin stuff

--- src/engine/game/battle/enemybattler.lua
+++ src/engine/game/battle/enemybattler.lua
@@ -734,6 +734,10 @@ function EnemyBattler:onDodge(battler, attacked) end
 ---@param damage?    number
 ---@param battler?   PartyBattler
 function EnemyBattler:onDefeat(damage, battler)
+    if self.killable and Game:getFlag("can_kill") then
+        self:onDefeatFatal(damage, battler)
+        return
+    end
     if self.exit_on_defeat then
         self:onDefeatRun(damage, battler)
     else
