8bbee68035d5720b5d43b2e46381b39649765b52
Simbel0 <simbel.user@gmail.com>
2024-09-02 22:35:14 +0200

    Removed my test repos

--- src/engine/vendcust.lua
+++ src/engine/vendcust.lua
@@ -20,11 +20,4 @@ GITHUB_ORGANISATION = nil
 
 -- Additional DLCs that aren't in the organisation above for some reason
 -- (like the organisation not existing yet)
-GITHUB_REPOS = {
-	Simbel0={
-		"Kristal-Wii-BIOS-Mod",
-		"bikini-spamton",
-		"Dark-place",
-		"Temple-Of-Creativity"
-	}
-}
+GITHUB_REPOS = {}
