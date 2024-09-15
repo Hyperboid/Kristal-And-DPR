58ec12ad14d8efbdda0ddcc45bdabad27dcaf64e
AcousticJamm <jasmicheals@gmail.com>
2024-09-15 09:09:44 -0400

    Flee text getter

--- src/engine/game/battle.lua
+++ src/engine/game/battle.lua
@@ -749,7 +749,122 @@ function Battle:onStateChange(old,new)
         end
 
         self.defending_begin_timer = 0
-    end
+    elseif new == "FLEE" then
+        self.current_selecting = 0
+		local flee_complete = false
+
+        if self.tension_bar then
+            self.tension_bar:hide()
+        end
+
+        for _,battler in ipairs(self.party) do
+            battler:setSleeping(false)
+            battler.defending = false
+            battler.action = nil
+
+            if battler.chara:getHealth() <= 0 then
+                battler:revive()
+                battler.chara:setHealth(1)
+            end
+
+			battler:setAnimation("battle/hurt")
+
+            Assets.playSound("defeatrun")
+
+			local sweat = Sprite("effects/defeat/sweat")
+			sweat:setOrigin(0.5, 0.5)
+			sweat:setScale(0.5, 0.5)
+			sweat:play(5/30, true)
+			sweat.layer = 100
+			battler:addChild(sweat)
+
+			Game.battle.timer:after(15/30, function()
+				sweat:remove()
+				battler:getActiveSprite().run_away_2 = true
+				flee_complete = true
+			end)
+
+            local box = self.battle_ui.action_boxes[self:getPartyIndex(battler.chara.id)]
+            box:resetHeadIcon()
+        end
+        
+        -- self.money = self.money + (math.floor(((Game:getTension() * 2.5) / 10)) * Game.chapter)
+
+        for _,battler in ipairs(self.party) do
+            for _,equipment in ipairs(battler.chara:getEquipment()) do
+                self.money = math.floor(equipment:applyMoneyBonus(self.money) or self.money)
+            end
+        end
+
+        self.money = math.floor(self.money)
+
+        self.money = self.encounter:getVictoryMoney(self.money) or self.money
+        self.xp = self.encounter:getVictoryXP(self.xp) or self.xp
+        -- if (in_dojo) then
+        --     self.money = 0
+        -- end
+
+        Game.money = Game.money + self.money
+        Game.xp = Game.xp + self.xp
+
+        if (Game.money < 0) then
+            Game.money = 0
+        end
+        
+        local earn_text = ""
+        if self.money ~= 0 or self.xp ~= 0 then
+            earn_text = "* Ran away with " .. self.xp .. " EXP and " .. self.money .. " "..Game:getConfig("darkCurrencyShort").."."
+        end
+            
+        if self.used_violence and Game:getConfig("growStronger") then
+            local stronger = "You"
+
+            for _,battler in ipairs(self.party) do
+                Game.level_up_count = Game.level_up_count + 1
+                battler.chara:onLevelUp(Game.level_up_count)
+
+                if battler.chara.id == Game:getConfig("growStrongerChara") then
+                    stronger = battler.chara:getName()
+                end
+            end
+
+            earn_text = "* Ran away with " .. self.money .. " "..Game:getConfig("darkCurrencyShort")..".\n* "..stronger.." became stronger."
+
+            Assets.playSound("dtrans_lw", 0.7, 2)
+            --scr_levelup()
+        end
+        
+        local flee_text = "* "
+		
+		local flee_list = {
+			"I'm outta here.",
+			"I've got better to do.",
+			"Escaped...",
+			"Don't slow me down."
+		}
+		
+        for _,battler in pairs(Game.battle.party) do
+            for _,text in pairs(battler.chara:getFleeText()) do
+                table.insert(flee_list, text)
+            end
+        end
+		
+        if earn_text == "" then
+            flee_text = flee_text .. Utils.pick(flee_list)
+        else
+            flee_text = earn_text
+        end
+		
+        self:battleText(flee_text, function()
+			for _,battler in ipairs(self.party) do
+				battler:getActiveSprite().run_away_2 = false
+				battler.x = battler.x - 240
+			end
+            self:setState("TRANSITIONOUT")
+            self.encounter:onBattleEnd()
+            return true
+        end)
+	end
 
     -- List of states that should remove the arena.
     -- A whitelist is better than a blacklist in case the modder adds more states.
--- src/engine/game/common/data/partymember.lua
+++ src/engine/game/common/data/partymember.lua
@@ -417,6 +417,8 @@ function PartyMember:getMenuIconOffset() return unpack(self.menu_icon_offset or 
 
 function PartyMember:getGameOverMessage() return self.gameover_message end
 
+function PartyMember:getFleeText() return self.flee_text end
+
 -- Functions / Getters & Setters
 
 --- Heals this party member
