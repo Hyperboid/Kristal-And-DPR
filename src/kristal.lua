84a07c13925fb6580e686bc3ea4aaa5ebf07e9f8
SadDiamondMan <139525343+SadDiamondMan@users.noreply.github.com>
2024-10-04 17:21:10 -0400

    AAAAAAAAAAAA
    ctrl + n reloads noel_spawn.lua
    
    added more noel + cliffside + warstalker enemy

--- src/kristal.lua
+++ src/kristal.lua
@@ -63,8 +63,25 @@ function Kristal.fetch(url, options)
     Kristal.HTTPS.next_key = Kristal.HTTPS.next_key + 1
     return true
 end
+    
+function Kristal.reloadnoel()
+    -- If you don't know what this is for, then don't touch it!!!
+    
+    package.loaded["src.engine.game.noel.noel_spawn"] = nil 
+    Noel = require("src.engine.game.noel.noel_spawn")      
+    if Noel:loadNoel() then
+        Kristal.noel = true
+    else 
+        Kristal.noel = false
+    end
+end
+
+Kristal.reloadnoel()
+
+end
 
 function love.load(args)
+    Kristal.reloadnoel()
     --[[
         Launch args:
             --wait: Pauses the load screen until a key is pressed
@@ -532,6 +549,9 @@ function Kristal.onKeyPressed(key, is_repeat)
                     Kristal.returnToMenu()
                 end
             end
+        elseif key == "n" and Input.ctrl() and not console_open then
+            Kristal.reloadnoel()
+            print("reloaded noel_spawn.lua")
         end
     end
 
@@ -1019,6 +1039,8 @@ function Kristal.clearModState()
     -- Restore assets and registry
     Assets.restoreData()
     Registry.initialize()
+
+    Kristal.reloadnoel()
 end
 
 --- Exits the current mod and returns to the Kristal menu.
