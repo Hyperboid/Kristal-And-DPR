de3b724fd0393e479ee1415d74fa4967588bcf85
hyperboid <hyperboid@nullanoid.net>
2024-11-01 22:32:39 -0400

    Rewrite darkenter.lua and restore LoadingDarkTransition functionality

--- mods/dpr_main/scripts/world/cutscenes/darkenter.lua
+++ mods/dpr_main/scripts/world/cutscenes/darkenter.lua
@@ -2,89 +2,27 @@
 
 ---@param cutscene WorldCutscene
 return function(cutscene)
-    -- Landing positions based on party size. Hardcoded because screw me ig
-    local landing = {
-        {
-            {460, 680}
-        },
-        {
-            {500, 680},
-            {420, 680}
-        },
-        {
-            {460, 680},
-            {420, 720},
-            {500, 720}
-        },
-        { -- Juuuust in case we end up using MoreParty.
-            {500, 680},
-            {420, 680},
-            {380, 720},
-            {540, 720}
-        },
-    }
-
-
-
-    -- Black background
-    local blackbg = Sprite("kristal/doorblack")
-    blackbg.x, blackbg.y = Game.world.map:getMarker("spawn")
-    blackbg:setOrigin(0.5)
-    blackbg:setScale(10)
-    blackbg.layer = 0.5
-    Game.world:addChild(blackbg)
-
+    local markx,marky = Game.world.map:getMarker("landing")
+    cutscene:after(function()
+        DTRANS = nil
+    end)
+    
     cutscene:detachCamera()
+    Game.world.camera:setPosition(markx, marky)
     cutscene:detachFollowers()
     Kristal.hideBorder(1)
-
-    -- Get every party member and move them up offscreen
-    local party = {}
-    for i, chara in ipairs(Game.party) do
-        table.insert(party, cutscene:getCharacter(Game.party[i].id))
-        party[i].sprite:set("jump_ball")
-        party[i].layer_old = party[i].layer
-        party.layer = WORLD_LAYERS["ui"]
-        party[i].x = landing[#Game.party][i][1]
-        party[i].y = landing[#Game.party][i][2] - SCREEN_HEIGHT/2 - 100 -- Juuuust in case
+    local transition = LoadingDarkTransition(300, {
+        resuming = true,
+        character_data = DTRANS
+    })
+    transition.layer = WORLD_LAYERS["top"]
+    local waiting = true
+    transition.end_callback = function(trans, data)
+        waiting = false
     end
-
-    -- Drop everybody down
-    for i, chara in ipairs(party) do
-        chara:slideTo(landing[#party][i][1], landing[#party][i][2], 0.5)
-    end
-
-    cutscene:wait(0.5)
-
-    -- We've hit the ground.
-    Game.stage:shake(8)
-    Assets.playSound("dtrans_flip")
-    for i, chara in ipairs(party) do
-        chara.sprite:set("landed")
-    end
-    cutscene:wait(1)
-    Kristal.showBorder(0.75)
-    Assets.playSound("him_quick")
-    blackbg:fadeOutAndRemove(0.75)
-    cutscene:wait(0.75)
-    for i, chara in ipairs(party) do
-        chara.sprite:play(4/30, false)
-        chara.layer = chara.layer_old
-    end
-
-    cutscene:wait(0.5)
-    cutscene:resetSprites()
+    Game.world:addChild(transition)
+    cutscene:wait(function() return not waiting end)
     cutscene:attachCamera()
     cutscene:interpolateFollowers()
     cutscene:attachFollowers()
-    DTRANS = false
-
-
-
-
-
-
-
-
-
 end
