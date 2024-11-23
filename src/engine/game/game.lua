950d9bb86237186e18b5e9df6a6f88cc5ea61e2a
SadDiamondMan <139525343+SadDiamondMan@users.noreply.github.com>
2024-11-23 16:08:11 -0400

    used .lua instead of .json

--- src/engine/game/game.lua
+++ src/engine/game/game.lua
@@ -1316,7 +1316,7 @@ end
 function Game:getGlobalFlag(flag, default)
     local flags
 
-    if love.filesystem.getInfo("saves/global_flags.lua") then
+    if love.filesystem.getInfo("saves/global_flags.json") then
         flags = JSON.decode(love.filesystem.read("saves/global_flags.json"))
     else
         return false
@@ -1332,7 +1332,7 @@ end
 
 function Game:setGlobalFlag(flag_name, value)
     local data
-    if love.filesystem.getInfo("saves/global_flags.lua") then
+    if love.filesystem.getInfo("saves/global_flags.json") then
         data = JSON.decode(love.filesystem.read("saves/global_flags.json"))
     else
         data = {}
